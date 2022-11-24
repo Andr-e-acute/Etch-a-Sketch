84737a8680d790eabff8f6176deb12a71a7f51b2
André-git <andre.potthast.ap@gmail.com>
2022-11-24 11:33:33 +0100

    simple pixelGrid done without user Input for size

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ const gridX = 16;
 const gridY = 16;
 const gridContainer = document.querySelector("#gridContainer");
 
+//test div for finding the right size
 
+//function one line of the grid
+function createLine(sizeX,indexY) {
+   
+    for (let index = 1; index <= sizeX; index++) {
+      let gridPixel = document.createElement("div");
+      gridPixel.classList.add("gridPixel");
+      
+      //TODOcreate Id for coordinates
+      gridPixel.setAttribute('id',`y${indexY}x${index}`);
+      gridContainer.appendChild(gridPixel);
+    }
+  }
+  
+  //create gridY times of lines
+  for (let index = 1; index <= gridY; index++){
+    console.log(index)
+      createLine(gridX,index);
+    
+  }
