1b6d1f38560b13ee0307c512f37e35ae8f467deb
André-git <andre.potthast.ap@gmail.com>
2022-11-30 14:47:32 +0100

    added color Pickers for paint Background

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
+//start size TODO Remove after getting values from sliders
 const gridX = 16;
 const gridY = 16;
 const gridContainer = document.querySelector("#gridContainer");
+//root for css varibles
+const root = document.querySelector(":root");
+const paintPicker = document.querySelector("#paintColor");
 
-//test div for finding the right size
+
+//prevent the dragging grab ? default
+gridContainer.addEventListener("mousemove",(e)=>{
+  e.preventDefault()
+})
+
 
 //function one line of the grid
-function createLine(sizeX,indexY) {
-   
-    for (let index = 1; index <= sizeX; index++) {
-      let gridPixel = document.createElement("div");
-      gridPixel.classList.add("gridPixel");
-      
-      //TODOcreate Id for coordinates
-      gridPixel.setAttribute('id',`y${indexY}x${index}`);
-      gridContainer.appendChild(gridPixel);
-    }
+function createLine(sizeX, indexY) {
+  for (let index = 1; index <= sizeX; index++) {
+    let gridPixel = document.createElement("div");
+    gridPixel.classList.add("gridPixel");
+    gridPixel.setAttribute("id", `y${indexY}x${index}`);
+    gridPixel.style.width = `calc(100%/${gridX})`;
+
+    //mousemove for painting and mousedown for singlePixel
+    gridPixel.addEventListener("mousemove", paintPixel);
+    gridPixel.addEventListener('mousedown',paintPixel)
+    gridContainer.appendChild(gridPixel);
   }
-  
-  //create gridY times of lines
-  for (let index = 1; index <= gridY; index++){
-    console.log(index)
-      createLine(gridX,index);
-    
-  }
+}
+
+//create CanvasGrid gridY times of lines
+for (let index = 1; index <= gridY; index++) {
+  createLine(gridX, index);
+}
+
+//
+function paintPixel(e) {
+  if(e.buttons===1){
+  e.target.style.backgroundColor = paintPicker.value;
+  e.preventDefault();
+  e.stopPropagation()
+  console.log(e.buttons)
+}
+}
+/*TODO
+add slider for Gridsize
+manuel input of Gridsize??
+remove double gridlines
+
+//features
+eraser
+clear
+shading 
+ligthning
+pen pressure?
+
+*/
