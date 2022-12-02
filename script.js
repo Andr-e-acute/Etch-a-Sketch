720ce9cc933f3c396a813c5acf1f122f68fed919
André-git <andre.potthast.ap@gmail.com>
2022-12-02 15:11:02 +0100

    Slider partial working need to refractor to remove the parent Grid

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 //start size TODO Remove after getting values from sliders
-const gridX = 16;
-const gridY = 16;
+
 const gridContainer = document.querySelector("#gridContainer");
-//root for css varibles
+//root for css variables
 const root = document.querySelector(":root");
 const paintPicker = document.querySelector("#paintColor");
+const gridSizeSlider = document.querySelector('#gridSizeSlider');
 
+
+
+
+gridSizeSlider.addEventListener('change',(e)=>{createGrid(e.target)})
 
 //prevent the dragging grab ? default
 gridContainer.addEventListener("mousemove",(e)=>{
   e.preventDefault()
 })
 
+createGrid(gridSizeSlider);
+
+function createGrid(size){
+  const gridX = size.value;
+  const gridY =size.value;
+
+  for (let index = 1; index <= gridY; index++) {
+    createLine(gridX, index);
+  }
+
+}
 
 //function one line of the grid
 function createLine(sizeX, indexY) {
+  console.log('createLine')
   for (let index = 1; index <= sizeX; index++) {
     let gridPixel = document.createElement("div");
     gridPixel.classList.add("gridPixel");
     gridPixel.setAttribute("id", `y${indexY}x${index}`);
-    gridPixel.style.width = `calc(100%/${gridX})`;
+    gridPixel.style.width = `calc(100%/${sizeX})`;
 
     //mousemove for painting and mousedown for singlePixel
     gridPixel.addEventListener("mousemove", paintPixel);
@@ -28,12 +44,6 @@ function createLine(sizeX, indexY) {
   }
 }
 
-//create CanvasGrid gridY times of lines
-for (let index = 1; index <= gridY; index++) {
-  createLine(gridX, index);
-}
-
-//
 function paintPixel(e) {
   if(e.buttons===1){
   e.target.style.backgroundColor = paintPicker.value;
@@ -42,16 +52,20 @@ function paintPixel(e) {
   console.log(e.buttons)
 }
 }
+//create CanvasGrid gridY times of lines
+
+
+//
 /*TODO
 add slider for Gridsize
 manuel input of Gridsize??
 remove double gridlines
 
 //features
-eraser
+eraser`
 clear
-shading 
-ligthning
+shading add Modus 
+lighten -modus
 pen pressure?
 
 */
