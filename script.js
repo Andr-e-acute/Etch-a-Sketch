1efeda0857e89cba01b60c993927ad7c587426e4
André-git <andre.potthast.ap@gmail.com>
2022-12-04 12:24:40 +0100

    fixed grid size change bug wrong direction

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ const backgroundColor=document.querySelector("#background")
 const gridSizeSlider = document.querySelector('#gridSizeSlider');
 const gridSizeX = document.querySelector('#xSize');
 const gridSizeY = document.querySelector('#ySize');
+const clearButton = document.querySelector("#clearButton");
 
-backgroundColor.addEventListener('change',(e)=>{root.style.setProperty(e.target.name, e.target.value)})
+clearButton.addEventListener('click',()=>createGrid(gridSizeX.value,gridSizeY.value))
+backgroundColor.addEventListener('input',(e)=>{root.style.setProperty(e.target.name, e.target.value)})
 gridSizeSlider.addEventListener('change',(e)=>{createGrid(e.target.value,e.target.value)})
 gridSizeX.addEventListener('change',(e)=>{constrainGridSize(e.target.value,gridSizeY.value)})
-gridSizeY.addEventListener('change',(e)=>(constrainGridSize(gridSizeY.value,e.target.value)))
+gridSizeY.addEventListener('change',(e)=>(constrainGridSize(gridSizeX.value,e.target.value)))
 //prevent the dragging grab ? default
 gridContainer.addEventListener("mousemove",(e)=>{
   e.preventDefault()
 })
 function constrainGridSize(x,y){
+  
  let xSize=x;
  let ySize= y;
   if(x >MAX_SIZE){
@@ -28,6 +31,7 @@ function constrainGridSize(x,y){
     ySize = MAX_SIZE
   }
   console.log(xSize)
+  console.log(ySize)
   
 createGrid(xSize,ySize)
 }
@@ -40,8 +44,8 @@ function createGrid(sizeX,sizeY){
   gridContainer.remove();
   gridContainer = gridContainer.cloneNode()
 
-  for (let index = 1; index <= sizeX; index++) {
-    createRow(sizeY, index);
+  for (let index = 1; index <= sizeY; index++) {
+    createRow(sizeX);
   }
 
   gridParent.append(gridContainer)
@@ -49,7 +53,7 @@ function createGrid(sizeX,sizeY){
 }
 
 //function one Row of the grid
-function createRow(sizeY, indexX) {
+function createRow(sizeY) {
   
   for (let index = 1; index <= sizeY; index++) {
     let gridPixel = document.createElement("div");
@@ -78,13 +82,17 @@ createGrid(gridSizeSlider.value,gridSizeSlider.value);
 
 //
 /*TODO
+changing the size should only change the size and not delete
+for this:
+-every row or column should be a neseted in a dif...
+-update function must
+  -change the style width are the inline calc update ??
 
 
 remove double gridlines
 
 //features
 eraser`
-clear
 shading add Modus 
 lighten -modus
 pen pressure?
